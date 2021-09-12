b45c67a38d2f8bad5af642266e559a5aa4797931
Moshe Buznah <mosheb2000@gmail.com>
2021-09-12 11:00:08 +0300

    develop before merge

--- client/Android-studio/app/src/main/java/com/android_project_client/Login.java
+++ client/Android-studio/app/src/main/java/com/android_project_client/Login.java
@@ -2,14 +2,26 @@ package com.android_project_client;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
+    TextView signUpTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        signUpTv = findViewById(R.id.signUp);
 
+        signUpTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent go = new Intent(Login.this, signUp.class);
+                startActivity(go);
+            }
+        });
     }
 }
